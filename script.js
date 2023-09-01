ee9bfb3b25c12713eadcb8d5dcd258927cbfd243
YinHao <shdic2@163.com>
2023-09-01 11:38:59 +0800

    add

--- script.js
+++ script.js
@@ -6,6 +6,17 @@ handleDom.onclick = async function () {
   let inputVal = inputDom.value || "";
   inputVal = inputVal.trim();
   if (!inputVal) return;
-  var result = await Terser.minify(inputVal, { sourceMap: false });
+  // 配置选项
+  const options = {
+    compress: {
+      dead_code: true, // 删除无用的代码
+      //drop_console: true, // 删除 console 语句
+    },
+    mangle: true, // 混淆变量名
+    output: {
+      comments: false, // 去除注释
+    },
+  };
+  var result = await Terser.minify(inputVal, options);
   outputDom.value = result.code;
 };
